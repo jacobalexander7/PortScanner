5fab91d9fbbde046fdc878a02b291760c3117ee2
Jacob Alexander Coomer <81890294+jacobalexander7@users.noreply.github.com>
2022-02-14 18:13:41 -0500

    Added scanner

---
+++ coomerscan.py
@@ -0,0 +1,43 @@
+# Jacob Coomer
+# This tool scans ports 1-65535 on a target address.
+# This tool uses the socket, sys, and datetime packages.
+# To run, use the command <py coomerscan.py ip>.
+# You can break using Ctrl+C.
+import socket
+import sys
+from datetime import datetime
+
+
+# Scanning function
+def connect(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.1)
+    info = sock.connect_ex((host, port))
+    if info == 0:
+        print("Port " + str(port) + " Open -- " + str(datetime.now()))
+    sock.close()
+
+
+# Check for IP argument in the command
+if len(sys.argv) < 3:
+    print("No Target. Enter in format <py coomerscan.py ip>")
+    sys.exit()
+target = sys.argv[1] if sys.argv[1] else None
+
+target = socket.gethostbyname(target)
+try:  # try to scan all ports
+    for i in range(1, 65356):
+        connect(target, i)
+
+except socket.gaierror:  # IP not valid
+    print("Address information is not valid.")
+    sys.exit()
+except socket.error:  # Connection issue
+    print("Unable to connect. Host may still be valid.")
+    sys.exit()
+except KeyboardInterrupt:  # User Exit
+    print("Ctrl C -> Exit")
+    sys.exit()
+
+print("Done")
+sys.exit()
